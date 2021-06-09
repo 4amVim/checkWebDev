bc60bda974065e63df89c23c0252652baa7c4c0c
igloo <4429609+4amvim@users.noreply.github.com>
2021-06-09 06:02:19 +0530

    refactor: :recycle: almost done

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
+//*The types we have over at firebase
 const lists=['Chores','Work','Critical','Misc']
 
 //*Takes in the [name] of the list you want to extract from fetched [rawData]
 //*And does callback things with each of it's key-value pairs
-function extractList ( name, rawData, callback ) {
-    const list = rawData.fields[name].mapValue.fields;
+function extractList ( name, data, callback ) {
+    const list = data.fields[name].mapValue.fields;
     for ( const todo in list ) {
         console.log( list[todo].booleanValue );
         console.log( todo );
